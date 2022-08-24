f88ebf69e0f3efb8ca77f52682a60a0731b9fc34
user <sivtsovatanya1985@gmail.com>
2022-08-24 16:20:07 +0300

    Added firebase in project;

--- src/components/name-page/NamePage.js
+++ src/components/name-page/NamePage.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import './NamePage.css';
 
 const NamePage = () => {
+	const [userName, setUserName] = useState('');
+	const handleChange = (event) => {
+		setUserName(event.target.value);
+	}
+	const handleSubmit  = () => {
+		let users = {
+			userName: userName,
+		}
+		console.log(users);
+	}
 	return (
 		<div className='container'>
 			<div className='header'>
@@ -9,12 +19,17 @@ const NamePage = () => {
 			</div>
 			<div className='content'>
 				<h2>What is your name?</h2>
-				<input className='w-90' type='text' placeholder='enter name'/>
-				<button className='w-90 accept-button'>NEXT</button>
+				<input
+					className='w-90'
+					type='text'
+					placeholder='enter name'
+					value={userName}
+				    onChange={handleChange}
+				/>
+				<button onClick={handleSubmit} className='w-90 accept-button'>NEXT</button>
 			</div>
-
 		</div>
 	)
 }
-
 export default NamePage;
+
